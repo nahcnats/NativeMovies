55e69c7a189ae502b4eaac6e7e2d3e707cdce6f2
nahcnats <nahcnats@gmail.com>
2024-04-18 09:55:20 +0800

    RNConfig ios

---
+++ ios/RNConfig.h
@@ -0,0 +1,12 @@
+//
+//  RNConfig.h
+//  NativeMovies
+//
+//  Created by Stanley Chan on 18/04/2024.
+//
+
+#ifndef RNConfig_h
+#define RNConfig_h
+
+
+#endif /* RNConfig_h */
---
+++ ios/RNConfig.m
@@ -0,0 +1,8 @@
+//
+//  RNConfig.m
+//  NativeMovies
+//
+//  Created by Stanley Chan on 18/04/2024.
+//
+
+#import <Foundation/Foundation.h>
